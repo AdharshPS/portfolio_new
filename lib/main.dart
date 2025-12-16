cfb9dc27b0fa9dc76b6743b0e6ac1b7b3e988043
AdharshPS <adharshps000@gmail.com>
2025-12-16 17:29:16 +0530

    updated auto deploy with version

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,27 @@ import 'package:portfolio_new/screens/portfolio_screen.dart';
 import 'package:web/web.dart' as web;
 
 const env = String.fromEnvironment('ENV', defaultValue: 'dev');
+const buildVersion = String.fromEnvironment('BUILD_VERSION', defaultValue: '0');
 String get appTitle {
   return env == 'prod' ? 'Adharsh PS Portfolio' : 'Adharsh PS Portfolio (DEV)';
 }
 
 void setBrowserTitle() {
   web.document.title = appTitle;
+}
+
+void handleBuildVersionChange() {
+  try {
+    final storage = web.window.localStorage;
+    final stored = storage.getItem('build_version');
+
+    if (stored != buildVersion) {
+      storage.setItem('build_version', buildVersion);
+      web.window.location.reload();
+    }
+  } catch (_) {
+    // Ignore storage errors (private mode, etc.)
+  }
 }
 
 void main() {
