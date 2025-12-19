7863f646e7b948995eb2119cd448c1d5c6bf87f0
AdharshPS <AdharshPS@users.noreply.github.com>
2025-12-19 06:27:08 +0000

    deploy: 51520daf6327d0bfabae7c4e48887db60b8ac4c6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3525776875"
+    serviceWorkerVersion: "343287321"
   }
 });
